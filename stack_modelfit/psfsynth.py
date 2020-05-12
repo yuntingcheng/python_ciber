5c64fc5f538894f69111ee4ab9c1b4de839177b3
ytcheng <ytcheng0708@gmail.com>
2020-05-11 21:31:17 -0700

    20200511

--- stack_modelfit/psfsynth.py
+++ stack_modelfit/psfsynth.py
@@ -6,9 +6,9 @@ def run_psf_synth(inst, ifield, filt_order=3, savedata=True):
 
     fname = mypaths['alldat'] + 'TM'+ str(inst) + \
     '/psfdata_synth_%s.pkl'%(fieldnamedict[ifield])
-    with open(fname, "wb") as f:
+    with open(fname, "rb") as f:
         profdat = pickle.load(f)
-   
+
     data_maps = {1: image_reduction(1), 2: image_reduction(2)}
 
     psfdata_in = stack_psf(inst, data_maps[inst].stackmapdat,m_min=4, m_max=9,
