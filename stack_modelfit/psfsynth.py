0229026a4e67e29ed3f681db721c5dc956947c08
ytcheng <ytcheng0708@gmail.com>
2020-05-11 21:37:08 -0700

    20200511

--- stack_modelfit/psfsynth.py
+++ stack_modelfit/psfsynth.py
@@ -11,26 +11,26 @@ def run_psf_synth(inst, ifield, filt_order=3, savedata=True):
 
     data_maps = {1: image_reduction(1), 2: image_reduction(2)}
 
-    psfdata_in = stack_psf(inst, data_maps[inst].stackmapdat,m_min=4, m_max=9,
+    psfdata_out = stack_psf(inst, data_maps[inst].stackmapdat,m_min=4, m_max=9,
      ifield_arr=[ifield], Nsub_single=True, savedata=False, save_stackmap=False)
 
     # profdat = {}
-    # profdat['rbins'] = psfdata_in[ifield]['rbins']
-    # profdat['rbinedges'] = psfdata_in[ifield]['rbinedges']
-    # profdat['rsubbins'] = psfdata_in[ifield]['rsubbins']
-    # profdat['rsubbinedges'] = psfdata_in[ifield]['rsubbinedges']
+    # profdat['rbins'] = psfdata_out[ifield]['rbins']
+    # profdat['rbinedges'] = psfdata_out[ifield]['rbinedges']
+    # profdat['rsubbins'] = psfdata_out[ifield]['rsubbins']
+    # profdat['rsubbinedges'] = psfdata_out[ifield]['rsubbinedges']
     # profdat['filt_order'] = filt_order
 
     profdat['out'] = {}
     profdat['out']['m_min'] = 4
     profdat['out']['m_max'] = 9
-    profdat['out']['Nsrc'] = psfdata_in[ifield]['Nsrc']
-    profdat['out']['profcb'] = psfdata_in[ifield]['prof']
-    profdat['out']['profcb_err'] = psfdata_in[ifield]['prof_err']
-    profdat['out']['profcbsub'] = psfdata_in[ifield]['profsub']
-    profdat['out']['profcbsub_err'] = psfdata_in[ifield]['profsub_err']
-    profdat['out']['cov'] = psfdata_in[ifield]['cov']
-    profdat['out']['covsub'] = psfdata_in[ifield]['covsub']
+    profdat['out']['Nsrc'] = psfdata_out[ifield]['Nsrc']
+    profdat['out']['profcb'] = psfdata_out[ifield]['prof']
+    profdat['out']['profcb_err'] = psfdata_out[ifield]['prof_err']
+    profdat['out']['profcbsub'] = psfdata_out[ifield]['profsub']
+    profdat['out']['profcbsub_err'] = psfdata_out[ifield]['profsub_err']
+    profdat['out']['cov'] = psfdata_out[ifield]['cov']
+    profdat['out']['covsub'] = psfdata_out[ifield]['covsub']
 
     psfdata_mid = stack_psf(inst, data_maps[inst].stackmapdat, m_min=13, m_max=14,
      ifield_arr=[ifield], Nsub_single=True, savedata=False, save_stackmap=False)
