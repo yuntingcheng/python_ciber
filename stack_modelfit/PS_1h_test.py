48075c1588ab7283d381cf1f7b43b30b6d9a0fcc
ytcheng <ytcheng0708@gmail.com>
2020-09-08 11:20:08 -0700

    20200908

--- stack_modelfit/PS_1h_test.py
+++ stack_modelfit/PS_1h_test.py
@@ -103,7 +103,86 @@ def run_1h_power_spec_test(ibatch, m_max_vega=17, mag_th_psf = 20):
         with open(fname, "wb") as f:
             pickle.dump(data , f)
 
+def run_1h_power_spec_test_nopsf(ibatch, m_max_vega=17):
 
+    batch_size = 10
+    icat_arr = np.linspace(0, batch_size-1, batch_size) + ibatch*batch_size
+    icat_arr = icat_arr.astype(int)
+
+    mag_th = m_max_vega - 2.5*np.log10(1594./3631.)
+    make_srcmap_class1 = make_srcmap(1)
+    make_srcmap_class2 = make_srcmap(2)
+
+    data = {'icat_arr':icat_arr}
+    for icat in icat_arr:
+        print('icat %d'%icat)
+        data[icat] = {}
+        df = get_micecat_df(icat)
+        df['Fnu_I'] = 3631 * 10**(-df['I'] / 2.5)
+        df['Fnu_H'] = 3631 * 10**(-df['H'] / 2.5)
+        nuFnu_I = np.array(make_srcmap_class1._ABmag2Iciber(df['I'].copy()))
+        nuFnu_H = np.array(make_srcmap_class2._ABmag2Iciber(df['H'].copy()))
+        idx_b = np.where(df['I'] < mag_th)[0]
+        f_res1,f_res2 = np.ones(len(df)),np.ones(len(df))
+
+        start_time = time.time()
+        for i,idx in enumerate(idx_b):
+            if i%(len(idx_b)//10)==0:
+                print('Calculate residual flux of masked sources %d / %d (%.1f %%),t = %.2f min'\
+                      %(i, len(idx_b), i/len(idx_b)*100, (time.time()-start_time)/60))
+            dfi = df.iloc[idx]
+            mask,_ = MZ14_mask(1,np.array([dfi['x']]),np.array([dfi['y']]),
+                                 np.array([dfi['I']]),m_max_vega=m_max_vega,verbose=True)
+            make_srcmap_class1.ms = np.array([dfi['I']])
+            make_srcmap_class1.xls = np.array([dfi['x']])
+            make_srcmap_class1.yls = np.array([dfi['y']])
+            make_srcmap_class2.ms = np.array([dfi['I']])
+            make_srcmap_class2.xls = np.array([dfi['x']])
+            make_srcmap_class2.yls = np.array([dfi['y']])
+            make_srcmap_class1.ms_inband = np.array([dfi['I']])
+            make_srcmap_class2.ms_inband = np.array([dfi['H']])
+
+            srcmapb1 = make_srcmap_class1.run_srcmap(ptsrc=True, verbose=False)
+            srcmapb2 = make_srcmap_class2.run_srcmap(ptsrc=True, verbose=False)
+            f_res1[idx] = np.sum(srcmapb1[mask==1]) / nuFnu_I[idx]
+            f_res2[idx] = np.sum(srcmapb2[mask==1]) / nuFnu_H[idx]
+
+        df['Fnu_I_res'] = df['Fnu_I'].copy() * f_res1
+        df['Fnu_H_res'] = df['Fnu_H'].copy() * f_res2        
+        dfc = df.loc[df['flag_central']==0]
+        dfsum = df.groupby('unique_halo_id')[['Fnu_I_res','Fnu_H_res']].sum()
+        dfsum.rename(columns={'Fnu_I_res':'Fnu_I_res_sum','Fnu_H_res':'Fnu_H_res_sum'}, inplace=True)
+        dfc = dfc.join(dfsum, on='unique_halo_id', how='inner')
+        dfc['I'] = -2.5 * np.log10(dfc['Fnu_I_res_sum']/3631)
+        dfc['H'] = -2.5 * np.log10(dfc['Fnu_H_res_sum']/3631) 
+
+        for i,(name,dfi) in enumerate(zip(['full','cen'],[df,dfc])):
+            data[icat][name] = {}
+            xs, ys, ms = np.array(dfi['x']), np.array(dfi['y']), np.array(dfi['I'])
+
+            for inst in [1,2]:
+                data[icat][name][inst] = {}
+                ms_inband = np.array(dfi['I']) if inst==1 else np.array(dfi['H'])
+
+                print('making srcmap for %s TM %d'%(name,inst))
+                make_srcmap_class = make_srcmap_class1 if inst==1 else make_srcmap_class2
+
+                make_srcmap_class.ms = ms
+                make_srcmap_class.ms_inband = ms_inband
+                make_srcmap_class.xls = xs
+                make_srcmap_class.yls = ys
+                srcmapf = make_srcmap_class.run_srcmap_nopsf()
+                srcmap = srcmapf
+                l,Cl,Clerr = get_power_spec(srcmap)
+                data[icat][name][inst]['Cl'] = Cl
+                data[icat][name][inst]['Clerr'] = Clerr
+                Omega_pix = ((7*u.arcsec)**2).to(u.sr).value
+                data[icat][name][inst]['Clsh'] = np.var(srcmap)*Omega_pix*np.ones_like(Cl)
+        data['l'] = l 
+        fname = 'micecat_data/PS1h_test_ibatch%d_maskth%d_nopsf.pkl'%(ibatch,m_max_vega)
+        with open(fname, "wb") as f:
+            pickle.dump(data , f)
+        
 # def run_1h_power_spec_test(ibatch):
 #     batch_size = 10
 #     icat_arr = np.linspace(0, batch_size-1, batch_size) + ibatch*batch_size
